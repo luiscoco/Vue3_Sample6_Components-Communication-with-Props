154c8779559b553edd23bdda27e6d2f3227269e2
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-06-13 08:46:39 +0200

    first-commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+// Define a reactive property
+const message = ref("");
+</script>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Parent Component</h1>
+    <ChildComponent :message="parentMessage" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import ChildComponent from "./ChildComponent.vue";
+
+// Define a reactive property
+const parentMessage = ref("Hello from parent");
+</script>
